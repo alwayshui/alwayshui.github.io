6f10cec6be1237453cb308d6cb8851283c8a74cc
alwayshui <72642446+alwayshui@users.noreply.github.com>
2020-10-16 13:31:17 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as b
+import numpy as A
+import numpy as w 
+A=1
+b=0
+w=1
+x=np.arange(0,5*np.pi,0.01)
+y=A*np.cos(w*x+b)
+
+plt.plot(x,y)
+plt.show()
---
+++ 3.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as b
+import numpy as A
+import numpy as w 
+import numpy as a 
+import numpy as c
+import numpy as d
+a=2.718281828459
+d=10
+c=1
+b=0
+w=50
+x=np.arange(-1,1,0.01)
+A=d*pow(a,c*x)
+y=A*np.cos(w*x+b)
+
+plt.plot(x,y)
+plt.show()
